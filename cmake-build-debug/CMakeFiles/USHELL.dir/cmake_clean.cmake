048bf289e80726d8cb1af0f5f411a4dc9c3531c9
codefather <codefather.ru@gmail.com>
2020-04-14 19:28:29 +0300

    migrate from local

---
+++ cmake-build-debug/CMakeFiles/USHELL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/USHELL.dir/main.c.o"
+  "USHELL"
+  "USHELL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/USHELL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
